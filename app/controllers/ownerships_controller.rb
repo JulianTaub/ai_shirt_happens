5f353064aae252baf7025eb1dab44b0b31dc3194
JulianTaub <jtaub9503@gmail.com>
2015-05-25 17:44:14 -0400

    pulling everything together

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -1,10 +1,12 @@
 class OwnershipsController < ApplicationController
   def create
-    shirt = Shirt.find(params[:shirt_id])
-    current_user.shirts << shirt
-    current_user.like(shirt)
-    redirect_to shirt, notice: "#{shirt.name} has been added to your collection"
+    @shirt = Shirt.find(params[:shirt_id])
+    current_user.shirts << @shirt
+    current_user.like(@shirt)
+    @recommendations  = current_user.recommended_shirts(4)
+    redirect_to @shirt, notice: "#{shirt.name} has been added to your collection" 
   rescue ActiveRecord::RecordInvalid => e
-    redirect_to shirt, notice: e.message
+    redirect_to @shirt, notice: e.message 
   end
 end
+
